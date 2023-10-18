1ecfa3954f03275a07be0c7ed86992d2b00fff87
ReidLaptop <reidsrorick@gmail.com>
2023-10-18 12:51:18 -0500

    Diagnostic Questions first Javascript attempt

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ function copyToClipboard(){
         document.execCommand("copy");
         alert("Copied to clipboard!");
 }
+
+function handleCommentaryClick(verseNumber) {
+    // Define commentary logic based on the verse number
+    if (verseNumber === 7) {
+        // Handle commentary for Luke 7:50
+        console.log("Commentary for Luke 7:50");
+        // Add your specific commentary logic here
+    } else if (verseNumber === 23) {
+        // Handle commentary for Luke 23:40-43
+        console.log("Commentary for Luke 23:40-43");
+        // Add your specific commentary logic here
+    }
+}
